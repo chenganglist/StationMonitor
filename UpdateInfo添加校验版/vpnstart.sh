0c5641ef75c5705869b5728bdf281e8ebba5a12a
Sublime2Coding <1464125055@qq.com>
2016-05-16 22:56:52 +0800

    去除多余文件

--- UpdateInfo添加校验版/vpnstart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wvdial &
-sleep 10
-service xl2tpd restart
-sleep 10
-echo "c Wan" > /run/xl2tpd/l2tp-control
-
--- www/local/server.php
+++
@@ -1,7 +0,0 @@
-<?php
-	ini_set('soap.wsdl_cache_enabled', '0');
-	include_once('service.php');
-	$server=new SoapServer('soap.wsdl',array('soap_version' => SOAP_1_2));
-	$server->setClass("service");
-	$server->handle();
-?>
